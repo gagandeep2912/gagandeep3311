3ddecb14af1933e266c16dec9223459c1c19b09f
gagandeep2912 <121671501+gagandeep2912@users.noreply.github.com>
2023-01-05 15:44:28 +0530

    Add files via upload

---
+++ Temp.js
@@ -0,0 +1,7 @@
+
+        function convert(){
+            const celcius=document.getElementById('data').value;
+            const Fahreneit=(celcius*1.8)+32;
+            alert(("Answer")+Fahreneit);
+        }
+    
