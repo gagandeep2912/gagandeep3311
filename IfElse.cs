17fb4c888cafbacd7c691641392f3a61a14ca8ad
gagandeep2912 <121671501+gagandeep2912@users.noreply.github.com>
2023-01-06 16:33:37 +0530

    Add files via upload

---
+++ IfElse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+
+
+    class IfElse
+    {
+        static void Main(string[] args)
+            
+        {
+            int countnum = 0, countvar=0, countspl=0;
+            Console.WriteLine("Enter the string");
+            string str = Console.ReadLine();
+
+            char[] c = str.ToCharArray();
+            for(int i = 0; i < str.Length; i++)
+            {
+                char character = c[i];
+                if(character>='0' && character <='9')
+                {
+                    
+                    countnum++;
+                }
+                else if(character >= 'A' && character <= 'Z' || character>= 'a' && character <= 'z')
+                {
+                    countvar++;
+                }
+                else 
+                {
+                    countspl++;
+                }
+
+            }
+            Console.WriteLine("The number of digits present are:"+countnum);
+            Console.WriteLine("The number of alphabets present are:" + countvar);
+            Console.WriteLine("The number of sprcial characters p present are:" + countspl);
+            
+
+        }
+
+    }
+}
---
+++ TransposeArray.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+
+
+    class CreateArray { 
+        public static void Arrays(int size1,int size2)
+    {
+            
+           
+            int[,] ar =new int[size1,size2];
+            Console.WriteLine("Array created");
+           
+            Console.WriteLine("Eneter the values for the array");
+            for(int i = 0; i<ar.GetLength(0); i++)
+            {
+               
+                for(int j = 0; j <ar.GetLength(1); j++)
+                {
+                   //Console.WriteLine(j);
+                   // Console.WriteLine(i);
+                    ar[i,j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+
+            }
+            Console.WriteLine("The given array");
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < ar.GetLength(1); j++)
+                {
+                    Console.Write(ar[i,j]+" ");
+                }
+                Console.WriteLine();
+              
+            }
+            Console.WriteLine("The converted array");
+            for (int i = 0; i < ar.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < ar.GetLength(1); j++)
+                {
+                    Console.Write(ar[j, i] + " ");
+                }
+                Console.WriteLine();
+               
+            }
+
+
+
+
+        }
+
+    }
+
+    class TransposeArray
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] ar = { { },{ } };
+
+            Console.WriteLine("enter the size1");
+           int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the size2");
+           int b = Convert.ToInt32(Console.ReadLine());
+
+       
+            CreateArray.Arrays(a,b);
+        }
+       
+        
+    }
+}
